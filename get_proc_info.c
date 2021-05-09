bbb2dd4956fd7edf092c9450fcfdd2774229da3d
Minh Huy <huy.cse.9@gmail.com>
2021-05-09 11:29:41 +0700

    Sample and makefile

--- get_proc_info.c
+++ get_proc_info.c
@@ -10,34 +10,24 @@ struct task_struct *task_child;        /*    Structure needed to iterate through
 struct list_head *list;            /*    Structure needed to iterate through the list in each task->children struct    */
  
 module_param(PID, long, S_IRUSR);
-MODULE_PARM_DESC(PID, "PID of current process");
+MODULE_PARM_DESC(PID, "PID of process");
 
-int iterate_init(void)                    /*    Init Module    */
+int init_module(void)                    /*    Init Module    */
 {
     printk(KERN_INFO "Input argument pid %ld\n", PID);
-    //printk(KERN_INFO "%s","LOADING MODULE\n");    /*    good practice to log when loading/removing modules    */
-     
-    // for_each_process( task ){            /*    for_each_process() MACRO for iterating through each task in the os located in linux\sched\signal.h    */
-    //     printk(KERN_INFO "\nPARENT PID: %d PROCESS: %s STATE: %ld",task->pid, task->comm, task->state);/*    log parent id/executable name/state    */
-    //     list_for_each(list, &task->children){                        /*    list_for_each MACRO to iterate through task->children    */
-    //         task_child = list_entry( list, struct task_struct, sibling );    /*    using list_entry to declare all vars in task_child struct    */
-    //         printk(KERN_INFO "\nCHILD OF %s[%d] PID: %d PROCESS: %s STATE: %ld",task->comm, task->pid, /*    log child of and child pid/name/state    */
-    //             task_child->pid, task_child->comm, task_child->state);
-    //     }
-    //     printk("-----------------------------------------------------");    /*for aesthetics*/
-    // }    
+       
     return 0;
  
 }                /*    End of Init Module    */
      
-void cleanup_exit(void)        /*    Exit Module    */
+void cleanup_module(void)        /*    Exit Module    */
 {
     printk(KERN_INFO "%s","REMOVING MODULE\n");
  
 }                /*    End of Exit Module    */
  
-module_init(iterate_init);    /*    Load Module MACRO    */
-module_exit(cleanup_exit);    /*    Remove Module MACRO    */
+module_init(init_module);    /*    Load Module MACRO    */
+module_exit(cleanup_module);    /*    Remove Module MACRO    */
  
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("ITERATE THROUGH ALL PROCESSES/CHILD PROCESSES IN THE OS");
